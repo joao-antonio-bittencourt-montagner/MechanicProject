48aeac469e1ea8a248b7b62bd916730e044fffad
Joao <joaoantoniob.montagner@gmail.com>
2025-08-19 10:53:56 -0400

    second commit

--- src/main/Login.java
+++ src/main/Login.java
@@ -5,15 +5,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLayeredPane;
 
 public class Login extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -27,16 +26,21 @@ public class Login extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
+
 	public Login() {
+		int value = 100;
+		int widht = 900;
+		int height = 600;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(value, value, widht, height);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLayeredPane TelaDeBloqueio = new JLayeredPane();
+		TelaDeBloqueio.setBounds(0, 0, 884, 561);
+		contentPane.add(TelaDeBloqueio);
 
 	}
-
 }
